26545cbc0ec0131882c688b831e2f35247719c31
Estevan Delgado <estevand@cpp.edu>
2023-11-09 16:47:18 -0800

    added a background image to login page

--- app/src/main/java/com/example/android/wherethehoops/MainActivity.kt
+++ app/src/main/java/com/example/android/wherethehoops/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.example.android.wherethehoops
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowInsets
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 
+private const val SPLASH_SCREEN: Long = 5000L
 class MainActivity : AppCompatActivity() {
+    private lateinit var topAnim: Animation
+    private lateinit var bottomAnim: Animation
+    private lateinit var image: ImageView
+    private lateinit var logo: TextView
 
+
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+
+        val controller = window.insetsController
+
+        controller?.hide(WindowInsets.Type.systemBars())
+
         setContentView(R.layout.activity_main)
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+        image = findViewById(R.id.imageView)
+        logo = findViewById(R.id.textView)
+
+        image.startAnimation(topAnim)
+        logo.startAnimation(bottomAnim)
+
+        Handler().postDelayed({
+            val intent = Intent(this, Dashboard::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_SCREEN)
     }
 }
