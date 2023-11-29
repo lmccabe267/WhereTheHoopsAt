dfad4c89c51c9ae103485573d736bd60aa403351
Liam McCabe <89570377+lmccabe267@users.noreply.github.com>
2023-11-28 16:43:46 -0800

    added binding to project

--- app/src/main/java/com/example/android/wherethehoops/Dashboard.kt
+++ app/src/main/java/com/example/android/wherethehoops/Dashboard.kt
@@ -2,10 +2,20 @@ package com.example.android.wherethehoops
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.android.wherethehoops.databinding.ActivityDashboardBinding
 
 class Dashboard : AppCompatActivity() {
+    private lateinit var binding: ActivityDashboardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.checkInButton.setOnClickListener{view: View ->
+            Toast.makeText(this, "checked in", Toast.LENGTH_SHORT).show()
+        }
     }
 }
