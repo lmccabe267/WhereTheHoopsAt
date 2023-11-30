8a0b7bf8c7d7ea3056ad459495d83cced06bdd3b
Estevan Delgado <estevand@cpp.edu>
2023-11-30 10:50:15 -0800

    added notification

--- app/src/main/java/com/example/android/wherethehoops/TestCourt.kt
+++ app/src/main/java/com/example/android/wherethehoops/TestCourt.kt
@@ -1,16 +1,24 @@
 package com.example.android.wherethehoops
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class TestCourt : AppCompatActivity() {
     private lateinit var checkInButton: Button
     private lateinit var checkOutButton: Button
+    private lateinit var playerCountTextView: TextView
     private lateinit var sharedPreferences: SharedPreferences
+    private var playerCount: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,12 +26,17 @@ class TestCourt : AppCompatActivity() {
 
         checkInButton = findViewById(R.id.checkInButton)
         checkOutButton = findViewById(R.id.checkOutButton)
+        playerCountTextView = findViewById(R.id.playerCountTextView)
+
         sharedPreferences = getPreferences(Context.MODE_PRIVATE)
-        val checkedIn = sharedPreferences.getBoolean(KEY_CHECKED_IN, false)
-        updateButtonVisibility(checkedIn)
+        playerCount = sharedPreferences.getInt(KEY_PLAYER_COUNT, 0)
+
+        updatePlayerCountText()
+
         checkInButton.setOnClickListener {
             performCheckIn()
         }
+
         checkOutButton.setOnClickListener {
             performCheckOut()
         }
@@ -31,18 +44,57 @@ class TestCourt : AppCompatActivity() {
 
     private fun performCheckIn() {
         with(sharedPreferences.edit()) {
-            putBoolean(KEY_CHECKED_IN, true)
+            playerCount++
+            putInt(KEY_PLAYER_COUNT, playerCount)
             apply()
         }
+        updatePlayerCountText()
         updateButtonVisibility(true)
+
+        if (playerCount >= COURT_CAPACITY) {
+            sendCourtFullNotification()
+        }
+
         Toast.makeText(this, "Checked in", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun sendCourtFullNotification() {
+        createNotificationChannel()
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle("Court Full!")
+            .setContentText("The court is full! No more check-ins allowed.")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(NOTIFICATION_ID, builder.build())
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "CourtFullChannel"
+            val descriptionText = "Court Full Channel"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     private fun performCheckOut() {
         with(sharedPreferences.edit()) {
-            putBoolean(KEY_CHECKED_IN, false)
+            playerCount--
+            putInt(KEY_PLAYER_COUNT, playerCount)
             apply()
         }
+        updatePlayerCountText()
         updateButtonVisibility(false)
         Toast.makeText(this, "Checked out", Toast.LENGTH_SHORT).show()
     }
@@ -52,7 +104,14 @@ class TestCourt : AppCompatActivity() {
         checkOutButton.isEnabled = checkedIn
     }
 
+    private fun updatePlayerCountText() {
+        playerCountTextView.text = "Players: $playerCount"
+    }
+
     companion object {
-        private const val KEY_CHECKED_IN = "checked_in"
+        private const val KEY_PLAYER_COUNT = "player_count"
+        private const val COURT_CAPACITY = 1
+        private const val CHANNEL_ID = "court_full_channel"
+        private const val NOTIFICATION_ID = 1
     }
 }
