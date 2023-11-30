f9957fbfa109261d1e28a7dac00e27403ecf639f
Estevan Delgado <estevand@cpp.edu>
2023-11-29 17:24:54 -0800

    users only allowed to check in once

--- app/src/main/java/com/example/android/wherethehoops/Dashboard.kt
+++ app/src/main/java/com/example/android/wherethehoops/Dashboard.kt
@@ -1,21 +1,58 @@
 package com.example.android.wherethehoops
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 import android.widget.Toast
-import com.example.android.wherethehoops.databinding.ActivityDashboardBinding
+import androidx.appcompat.app.AppCompatActivity
 
 class Dashboard : AppCompatActivity() {
-    private lateinit var binding: ActivityDashboardBinding
+    private lateinit var checkInButton: Button
+    private lateinit var checkOutButton: Button
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
-        binding = ActivityDashboardBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        binding.checkInButton.setOnClickListener{view: View ->
-            Toast.makeText(this, "checked in", Toast.LENGTH_SHORT).show()
+        checkInButton = findViewById(R.id.checkInButton)
+        checkOutButton = findViewById(R.id.checkOutButton)
+        sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+        val checkedIn = sharedPreferences.getBoolean(KEY_CHECKED_IN, false)
+        updateButtonVisibility(checkedIn)
+        checkInButton.setOnClickListener {
+            performCheckIn()
         }
+        checkOutButton.setOnClickListener {
+            performCheckOut()
+        }
+    }
+
+    private fun performCheckIn() {
+        with(sharedPreferences.edit()) {
+            putBoolean(KEY_CHECKED_IN, true)
+            apply()
+        }
+        updateButtonVisibility(true)
+        Toast.makeText(this, "Checked in", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun performCheckOut() {
+        with(sharedPreferences.edit()) {
+            putBoolean(KEY_CHECKED_IN, false)
+            apply()
+        }
+        updateButtonVisibility(false)
+        Toast.makeText(this, "Checked out", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun updateButtonVisibility(checkedIn: Boolean) {
+        checkInButton.isEnabled = !checkedIn
+        checkOutButton.isEnabled = checkedIn
+    }
+
+    companion object {
+        private const val KEY_CHECKED_IN = "checked_in"
     }
 }
